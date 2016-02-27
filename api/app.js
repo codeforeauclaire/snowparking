069de81e0beefd09c3331556a34d83f274413207
Anthony <Anthony123272@gmail.com>
2016-02-26 19:21:34 -0600

    Proof of concept single api endpoint

--- api/app.js
+++ api/app.js
@@ -1,10 +1,7 @@
 'use strict';
 var express = require('express');
 var app = express();
-
-app.get('/', function(req, res) {
-	res.send('Hello World!');
-});
+var router = express.Router();
 
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
@@ -33,3 +30,12 @@ MongoClient.connect(url, function(err, db) {
 		db.close();
 	}
 });
+
+router.get('/', function(req, res) {
+	res.json({
+		'error': false,
+		'message': 'Hello World'
+	});
+});
+
+app.use('/', router);
