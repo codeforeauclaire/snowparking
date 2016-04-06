5dcb36f3ca4e08d8adbcc3e163fd0a89b69ad7f9
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-04-06 18:03:48 -0500

    Update vmsinstall.sh

--- api/vmsinstall.sh
+++ api/vmsinstall.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Update all software & install new
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y git mongodb
+#sudo apt-get update
+#sudo apt-get upgrade -y
+#sudo apt-get install -y git mongodb
 
 # Clone repository
-git clone https://github.com/codeforeauclaire/snowparking.git ~/
+#git clone https://github.com/codeforeauclaire/snowparking.git ~/
 
-# Install NVM
+# Install NVM (If doing this manually, exit and reconnect)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+# Source nvm so it works in this script (Installing should do this automatically)
+. ~/.nvm/nvm.sh
 
 # Get a node version from NVM
 nvm install v5.10.1
