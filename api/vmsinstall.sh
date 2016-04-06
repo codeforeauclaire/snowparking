7653d0c2c71b031a81c06327894a537b32fdafd6
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-04-06 18:20:56 -0500

    Update vmsinstall.sh

--- api/vmsinstall.sh
+++ api/vmsinstall.sh
@@ -6,13 +6,13 @@ sudo apt-get upgrade -y
 sudo apt-get install -y git mongodb
 
 # Clone repository
-git clone https://github.com/codeforeauclaire/snowparking.git ~/
+git clone https://github.com/codeforeauclaire/snowparking.git /root/snowparking
 
 # Install Node Version Manager (nvm)
 ## Basic install
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 ## Source nvm so it works in this script (If doing manually exit and reconnect instead)
-. ~/.nvm/nvm.sh
+. /root/.nvm/nvm.sh
 ## Get a node version from NVM
 nvm install v5.10.1
 
@@ -20,4 +20,4 @@ nvm install v5.10.1
 npm install -g nodemon
 
 # Install all packages for snowparking api
-(cd ~/snowparking/api && npm install --save)
+(cd /root/snowparking/api && npm install --save)
