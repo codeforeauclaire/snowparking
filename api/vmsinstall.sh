e319f2978828f8dd5808e035606deff92e9a827b
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-04-06 18:06:48 -0500

    Update vmsinstall.sh

--- api/vmsinstall.sh
+++ api/vmsinstall.sh
@@ -8,13 +8,12 @@
 # Clone repository
 #git clone https://github.com/codeforeauclaire/snowparking.git ~/
 
-# Install NVM (If doing this manually, exit and reconnect)
+# Install Node Version Manager (nvm)
+## Basic install
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-
-# Source nvm so it works in this script (Installing should do this automatically)
+## Source nvm so it works in this script (If doing manually exit and reconnect instead)
 . ~/.nvm/nvm.sh
-
-# Get a node version from NVM
+## Get a node version from NVM
 nvm install v5.10.1
 
 # Install nodemon (for monitoring file changes during development)
