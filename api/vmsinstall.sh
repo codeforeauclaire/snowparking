54f96d6571d981b384f005c73bd3d101e7b4955b
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-04-06 18:40:50 -0500

    Update vmsinstall.sh

--- api/vmsinstall.sh
+++ api/vmsinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-{ # this ensures the entire script is downloaded #
+{ # this ensures the entire script is downloaded and run #
 
 # Update all software & install new
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git mongodb
@@ -22,4 +22,4 @@ npm install -g nodemon
 # Install all packages for snowparking api
 (cd /root/snowparking/api && npm install --save)
 
-} # this ensures the entire script is downloaded #
+} # this ensures the entire script is downloaded and run #
